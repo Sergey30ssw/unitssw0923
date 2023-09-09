fb93406ae350c6429dde6feb6413c02aa611a33c
Sergey Smirnov <ssw30@yandex.ru>
2023-09-09 11:10:20 +0400

    Initial commit

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,38 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CashbackHackServiceTest {
+    @Test
+    public void TestAmountLessBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 700;
+
+        int actual = service.remain(amount);
+        int expected = 300;
+
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void TestAmountMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 4_100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        assertEquals(expected,actual);
+    }@Test
+    public void TestBoundaryEqualsAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1_000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected,actual);
+    }
+
+}
